65ce5f8f4ae2d94981373b6a3b52861faf07024b
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-11-15 22:10:34 +0900

    Initial Commit

---
+++ SwiftUICombineCamera/SwiftUICombineCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineCameraApp.swift
+//  SwiftUICombineCamera
+//
+//  Created by 1101249 on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
