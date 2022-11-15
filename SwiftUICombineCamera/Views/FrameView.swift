3db250c6b02ee35a31c83bec388233da7fa7f227
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-11-15 22:12:15 +0900

    init

---
+++ SwiftUICombineCamera/Views/FrameView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameView.swift
+//  SwiftUICombineCamera
+//
+//  Created by 1101249 on 11/15/22.
+//
+
+import SwiftUI
+
+struct FrameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameView()
+    }
+}
